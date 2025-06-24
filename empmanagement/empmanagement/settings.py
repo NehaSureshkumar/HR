3b27a5b39b315d3369e65df2ab91868b4e5b22e8
Neha <nehas@appglide.io>
2025-06-24 17:40:46 +0530

    new requirements.txt

--- empmanagement/empmanagement/settings.py
+++ empmanagement/empmanagement/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'rest_framework',
     'employee',
     'accounts',
     'ms_id_management',
